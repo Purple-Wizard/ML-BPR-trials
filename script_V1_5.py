147d375a513e005d68f8340e229a4182fb884770
markvinczehu <mark.vincze.hu@gmail.com>
2023-04-07 20:11:42 +0200

    cnn with worldstat

---
+++ script_V1_5.py
@@ -0,0 +1,176 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.models import Sequential
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+import optuna
+
+def main():
+    dataset_path = "dataset_128x128"
+    num_images = 20000 # 176000
+    epochs = 100
+    img_size = (64, 64)
+    
+    # Load and preprocess images
+    images = load_images(dataset_path, num_images)
+    images = preprocess_images(images, img_size)
+    x_train, x_test = split_data(images)
+
+    # Optimize using Optuna
+    study = optuna.create_study(direction="minimize")
+    study.optimize(lambda trial: objective(trial, x_train, x_test, epochs), n_trials=50)
+
+    # Display best trial
+    best_trial = study.best_trial
+    print(f"Best trial: {best_trial.number}")
+    print(f"Best value: {best_trial.value:.5f}")
+    print(f"Best parameters: {best_trial.params}")
+
+    # Train the model with the best hyperparameters
+    best_model, best_batch_size = create_model(best_trial)
+    history = train_model(best_model, x_train, x_test, epochs, best_batch_size)
+
+    # Evaluate and visualize the model
+    evaluate_model(history)
+    visualize_results(best_model, x_test)
+
+def load_images(path, num_images):
+    print("Loading images...")
+    images = []
+    image_count = 0
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.lower().endswith('.png'):
+                if image_count >= num_images:
+                    break
+                image = Image.open(os.path.join(root, file))
+                images.append(np.array(image))
+                image_count += 1
+    return np.array(images)
+
+# Preprocessing
+def preprocess_images(images, image_size=None):
+    print("Preprocessing images...")
+    
+    processed_images = []
+    for image in images:
+        img = Image.fromarray(image)
+        
+        if image_size is not None:
+            img = ImageOps.fit(img, image_size, Image.LANCZOS)
+        
+        processed_images.append(np.array(img))
+    
+    processed_images = np.array(processed_images)
+    processed_images = processed_images.astype("float32") / 255.0
+    return processed_images
+
+# Data splitting
+def split_data(images):
+    print("Splitting data...")
+    x_train, x_test = train_test_split(images, test_size=0.2, random_state=42)
+    return x_train, x_test
+
+# Encoder and decoder functions now accept n_filters as an argument
+def create_encoder(n_filters):
+    encoder = Sequential()
+    encoder.add(Conv2D(n_filters[0], (3, 3), activation="elu", padding="same", input_shape=(64, 64, 3)))
+    encoder.add(MaxPooling2D((2, 2), padding="same"))
+    encoder.add(Conv2D(n_filters[1], (3, 3), activation="elu", padding="same"))
+    encoder.add(MaxPooling2D((2, 2), padding="same"))
+    encoder.add(Conv2D(n_filters[2], (3, 3), activation="elu", padding="same"))
+    encoder.add(MaxPooling2D((2, 2), padding="same"))
+    return encoder
+
+def create_decoder(n_filters):
+    decoder = Sequential()
+    decoder.add(Conv2D(n_filters[2], (3, 3), activation="elu", padding="same", input_shape=(8, 8, n_filters[2])))
+    decoder.add(UpSampling2D((2, 2)))
+    decoder.add(Conv2D(n_filters[1], (3, 3), activation="elu", padding="same"))
+    decoder.add(UpSampling2D((2, 2)))
+    decoder.add(Conv2D(n_filters[0], (3, 3), activation="elu", padding="same"))
+    decoder.add(UpSampling2D((2, 2)))
+    decoder.add(Conv2D(3, (3, 3), activation="sigmoid", padding="same"))
+    return decoder
+
+# Create model function now accepts trial as an argument
+def create_model(trial):
+    print("Creating autoencoder model...")
+
+    # Retrieve hyperparameters from the trial
+    batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128])
+    learning_rate = trial.suggest_loguniform("learning_rate", 1e-4, 1e-2)
+
+    n_filters = [
+        trial.suggest_int(f"n_filters_layer_{i}", 16, 64) for i in range(3)
+    ]
+
+    encoder = create_encoder(n_filters)
+    decoder = create_decoder(n_filters)
+
+    model = Sequential([encoder, decoder])
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss="binary_crossentropy")
+    return model, batch_size
+
+# Training the model
+def train_model(model, x_train, x_test, epochs, batch_size):
+    print("Training the model...")
+    lr_scheduler = ReduceLROnPlateau(
+        monitor='val_loss', factor=0.1, patience=5, verbose=2, mode='min',
+            min_delta=0.0001, cooldown=0, min_lr=0.0000000001, max_lr=0.02
+    )
+    early_stopping = EarlyStopping(monitor="loss", patience=6)
+    history = model.fit(x_train, x_train, epochs=epochs, batch_size=batch_size, validation_data=(x_test, x_test), callbacks=[lr_scheduler, early_stopping])
+    return history
+
+# Evaluate the model
+def evaluate_model(history):
+    print("Evaluating the model...")
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+# Visualize the results
+def visualize_results(model, x_test):
+    print("Visualizing results...")
+    decoded_imgs = model.predict(x_test)
+
+    n = 10
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+    # Display original images
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(x_test[i])
+        plt.title("Original")
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # Display reconstructed images
+        ax = plt.subplot(2, n, i + 1 + n)
+        plt.imshow(decoded_imgs[i])
+        plt.title("Reconstructed")
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+def objective(trial, x_train, x_test, epochs):
+    model, batch_size = create_model(trial)
+    model.summary()
+
+    history = train_model(model, x_train, x_test, epochs, batch_size)
+    loss = history.history["val_loss"][-1]
+
+    return loss
+
+
+if __name__ == "__main__":
+    main()
